47cd03599c2dd1fbf753079802f5371470ad635c
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-03-25 23:47:43 -0300

    Update newCode.ino

--- newCode.ino
+++ newCode.ino
@@ -1,12 +1,14 @@
-int sensorEsquerda = A3;
-int sensorDireita = A4;
+const int sensorEsquerda = A3;
+const int sensorDireita = A4;
+const int sensorFrente = 7;
 
 
-int motorEsquerda = 7;
-int motorDireita = 4;
+const int motorEsquerda = 4;
+const int motorDireita = 2;
 
 int leituraEsquerda = 0;
 int leituraDireita = 0;
+int leituraFrente = 0;
 
 
 void setup() {
@@ -16,16 +18,39 @@ delay(100);
 }
 
 void loop() {
-  //delay pro tinkercad nao travar tranto
-  delay(20);
+  
+  //sensor frontal
+  
+  long duration, cm;
+
+
+  pinMode(sensorFrente, OUTPUT);
+  digitalWrite(sensorFrente, LOW);
+  delayMicroseconds(2);
+  digitalWrite(sensorFrente, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(sensorFrente, LOW);
+
+  pinMode(sensorFrente, INPUT);
+  duration = pulseIn(sensorFrente, HIGH);
+
+  cm = microsecondsToCentimeters(duration);
+  
+  
+  
+  //sensores laterais
   
   leituraEsquerda = analogRead(sensorEsquerda);
   leituraDireita = analogRead(sensorDireita);
+  leituraFrente = cm;
 
   Serial.print("Sensor esquerda: ");
   Serial.print(leituraEsquerda);
   Serial.print("  Sensor direita: ");
-  Serial.println(leituraDireita);
+  Serial.print(leituraDireita);
+  Serial.print(" Sensor Frente: ");
+  Serial.print(leituraFrente);
+  Serial.println("cm.");
 
    if(leituraEsquerda > 200 || leituraDireita > 200){
         analogWrite(motorEsquerda, 200);
@@ -35,4 +60,12 @@ void loop() {
         analogWrite(motorEsquerda, 0);
         analogWrite(motorDireita, 0);
    }
+  
+  //delay pro tinkercad nao travar tranto
+  delay(100);
+}
+
+
+long microsecondsToCentimeters(long microseconds) {
+  return microseconds / 29 / 2;
 }
