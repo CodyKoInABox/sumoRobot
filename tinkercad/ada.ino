594c6b3368f5dfe956bd09d57115f555bbf52689
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-04-05 22:38:58 -0300

    organize code in folders

---
+++ tinkercad/ada.ino
@@ -0,0 +1,43 @@
+int sensorEsquerda = A0;
+int sensorDireita = A1;
+
+
+int motorEsquerda = A3;
+int motorDireita = A4;
+
+int leituraEsquerda = 0;
+int leituraDireita = 0;
+
+
+void setup() {
+  
+Serial.begin(9600);
+delay(100);
+}
+
+void loop() {
+  
+  leituraEsquerda = analogRead(sensorEsquerda);
+  leituraDireita = analogRead(sensorDireita);
+
+  Serial.print("Sensor esquerda: ");
+  Serial.print(leituraEsquerda);
+  Serial.print("  Sensor direita: ");
+  Serial.println(leituraDireita);
+
+//  digitalWrite(motorEsquerda, HIGH);
+//  digitalWrite(motorDireita, HIGH);
+
+   if(leituraEsquerda > 200 || leituraDireita > 200){
+   //   digitalWrite(motorEsquerda, HIGH);
+   //   digitalWrite(motorDireita, HIGH);
+        analogWrite(motorEsquerda, 255);
+        analogWrite(motorDireita, 255);
+   }
+   else{
+   //   digitalWrite(motorEsquerda, LOW);
+   //   digitalWrite(motorDireita, LOW);
+        analogWrite(motorEsquerda, 0);
+        analogWrite(motorDireita, 0);
+   }
+}
