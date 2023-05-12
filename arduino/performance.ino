22b15b8977817e94946e0f82b267de6aedd27dec
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-05-11 23:17:44 -0300

    create performance code

---
+++ arduino/performance.ino
@@ -0,0 +1,45 @@
+#define SECRET_1 212
+#define SECRET_2 500
+#define SECRET_3 2500
+#define SECRET_4 40
+#define SECRET_5 980
+
+void setup(){
+   Serial.begin(9600);
+   digitalWrite(11, HIGH);
+   digitalWrite(7, HIGH);
+   bool firstTime = true;
+}
+
+void loop(){
+    if(analogRead(A0) > SECRET_5 || analogRead(A1) > SECRET_5){
+        digitalWrite(5, LOW);
+        digitalWrite(9, LOW);
+        if(analogRead(A2); < SECRET_4){
+            digitalWrite(6, LOW);
+            digitalWrite(10, LOW);
+            analogWrite(10, SECRET_1);
+            analogWrite(5, SECRET_1);
+            firstTime = true;
+        }else{
+            if(firstTime){
+                digitalWrite(10, LOW);
+                digitalWrite(5, LOW);
+                digitalWrite(9, HIGH);
+                digitalWrite(10, HIGH);
+                delay(SECRET_2);
+                digitalWrite(9, LOW);
+                digitalWrite(10, LOW);
+                girarOff(MotorDireitoTras, MotorEsquerdoFrente);
+            }
+            firstTime = false;
+            digitalWrite(10, HIGH);
+            digitalWrite(9, HIGH);
+        }
+    }else{
+        digitalWrite(10, LOW);
+        digitalWrite(9, HIGH);
+        digitalWrite(5, HIGH);
+        delay(SECRET_3);
+    }
+}
